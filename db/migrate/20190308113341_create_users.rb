1c060f8c0a37c10630d685ef6f0797ac3f830738
Merlino95 <leonardo.capozzi.1995@gmail.com>
2019-03-08 14:24:42 +0100

    created user model

---
+++ db/migrate/20190308113341_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.text :description
+      t.string :avatar_url
+
+      t.timestamps
+    end
+  end
+end
