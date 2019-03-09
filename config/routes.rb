9ffac9b99afa935d379b4f9ea9133bf1a06cd7f3
Ludovico Ottobre <ludovico.ottobre.1@gmail.com>
2019-03-09 19:18:14 +0100

    project/new form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #resources :messages
   #resources :user_stories
   #resources :reviews
-  #resources :projects
+  resources :projects
   get 'signup'  => 'users#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
