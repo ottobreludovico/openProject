a8daccdef0f68e849dcc78b4866bf639857021fa
Merlino95 <leonardo.capozzi.1995@gmail.com>
2019-03-09 21:04:48 +0100

    added login user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  root 'home#index'
   #resources :joins
   #resources :messages
   #resources :user_stories
   #resources :reviews
   resources :projects
   get 'signup'  => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'home' => 'home#index'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#delete'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
