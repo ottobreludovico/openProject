fb81799ed76d8cf9c70cd3e34c067a84a2efd09a
Merlino95 <leonardo.capozzi.1995@gmail.com>
2019-03-13 01:35:46 +0100

    added missing user attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_225939) do
+ActiveRecord::Schema.define(version: 2019_03_12_235336) do
 
   create_table "joins", force: :cascade do |t|
     t.integer "user_id"
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 2019_03_12_225939) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
