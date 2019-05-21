73b4a779c4d23a0765c3edb007fcf70e3402ef7b
Simone Marino <mr-mone@hotmail.it>
2019-05-21 14:29:52 +0200

    creating user profile

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,4 @@
+module UsersHelper
+  def full_name
+    current_user.first_name + " " + current_user.last_name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do 
+Rails.application.routes.draw do
+
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
@@ -17,6 +18,11 @@ Rails.application.routes.draw do
   get 'projects' => 'projects#index'
   root 'home#index'
   get 'home' => 'home#index'
+
+resources :users
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
